b28d5c55b1bec79684da935c9c3d3ffff7606d6c
Patrick Dolan <dolanp1992@gmail.com>
2022-06-20 19:49:04 -0700

    remove for attributes

--- src/Components/NewPostForm.js
+++ src/Components/NewPostForm.js
@@ -4,9 +4,9 @@ function NewPostForm() {
   return (
     <>
       <form>
-        <label for="title">Title:</label>
+        <label>Title:</label>
         <input type="text" name="title" id="title"></input>
-        <label for="post">Post:</label>
+        <label>Post:</label>
         <input type="text" name="post" id="post"></input>
         <button type="submit">Post</button>
       </form>
