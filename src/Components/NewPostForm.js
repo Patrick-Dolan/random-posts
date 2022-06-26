836c0a2ad41ade245828fb2219845ee996cdb02f
Patrick Dolan <dolanp1992@gmail.com>
2022-06-26 16:34:15 -0700

    add firestore

--- src/Components/NewPostForm.js
+++ src/Components/NewPostForm.js
@@ -1,9 +1,28 @@
 import React from "react";
+import Firebase from "./../firebase";
+import { getDoc, doc, addDoc, collection, Timestamp } from "firebase/firestore";
 
 function NewPostForm() {
+  
+  async function handleFormSubmission(e) {
+    e.preventDefault();
+
+    const colRef = collection(Firebase, "posts");
+    try {
+      const docRef = await addDoc(colRef, {
+        title: e.target.title.value,
+        post: e.target.post.value,
+        created: Timestamp.now()
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  };
+
   return (
     <>
-      <form>
+      <form onSubmit={handleFormSubmission}>
         <label>Title:</label>
         <input type="text" name="title" id="title"></input>
         <label>Post:</label>
